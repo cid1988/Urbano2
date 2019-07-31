e886c351342ea960c0e21e3f1105eb0c141c7083
Pablo Perello <perellopablo1988@gmail.com>
2019-07-31 16:23:57 -0300

    La pantalla de home con permisos

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -4,7 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 
 const poaRoutes: Routes = [
-  {path:'home',component:HomeComponent},
+  {path:'home',component:HomeComponent}
 ];
 
 @NgModule({
