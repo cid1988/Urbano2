4611fdbf1497c40059f119e4c205c560905faa96
Pablo Perello <perellopablo1988@gmail.com>
2019-08-15 12:46:28 -0300

    Actualizado modelo de la coleccion de proyecto

--- src/app/modules/poa/models/proyecto.ts
+++ src/app/modules/poa/models/proyecto.ts
@@ -3,19 +3,38 @@ import mongoose from 'mongoose';
 export interface Proyecto {
   _id: mongoose.Schema.Types.ObjectId;
   nombre: string;
-  idJurisdiccion: string;
-  proyectoPadre: string;
-  eliminado: boolean;
-  hijos: [];
-  codigo: string;
+  validado: boolean;
+  aprobado: boolean;
   descripcion: string;
-  titulo: string;
-  start: string;
+  codIdentificacion: string;//Codigo
+  proyectoPadre: string;//Subproyecto de
+  codPlanAnterior: string;//Plan anterior
+  anioPlanAnterior: string;//Plan anterior
+  dependencia: string;
+  proyectoInversion: string;//De inversion
+  cancelado: string;
+  enPresentacion: string;
+  etapaProyecto: string;//Etapa
+  contratista: string;
+  compromisoG: string;
+  metaProducto: string;
+  metaCuantificacion: string;
+  //Falta el campo de expediente
+  comunas: [];
+  responsables: string[];
+  //Partida presupuestaria
+  prioridadMinisterial: string;
+  fechaInicio: string;
+  monedaSolicitado: string;//Presupuesto oficial
+  presupuestoSolicitado: string;
+  prioridadJefatura: string;
+  fechaFin: string;
+  monedaGestion: string;//Presupuesto adjudicado
+  presupuestoGestion: string;
+  etiquetas: string[];
+  temas: [];
+  proyectoPrioritario: boolean;
+  coordenadas: {lat: string, lng: string};
+  idJurisdiccion: string;
+  eliminado: boolean;
 }
-
-// export class Proyecto {
-//   constructor(
-//     public _id:string,
-//     public nombre:string
-//   ){}
-// }
