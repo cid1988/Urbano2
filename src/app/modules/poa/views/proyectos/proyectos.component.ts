43e7da1dbe5369b3e228e64c4d13f4cd10578491
Pablo Perello <perellopablo1988@gmail.com>
2019-11-19 16:20:04 -0300

    POA - Ahora se tienen en cuenta los permisos para cargar los listados de areas en la portada principal

--- src/app/modules/poa/views/proyectos/proyectos.component.ts
+++ src/app/modules/poa/views/proyectos/proyectos.component.ts
@@ -6,6 +6,7 @@ import { Plan } from '../../models/plan';
 import { UserService } from 'src/app/modules/administrador/services/user/user.service';
 import { NgxPermissionsService } from 'ngx-permissions';
 import { AutenticacionService } from 'src/app/shared-modules/login/services/autenticacion/autenticacion.service';
+import { OrganigramaService } from 'src/app/modules/organigrama/services/organigrama.service';
 
 @Component({
   selector: 'app-proyectos',
@@ -16,12 +17,12 @@ export class ProyectosComponent implements OnInit {
 
   plan: Plan;//De entrada traer siempre el ultimo
   planes = [];
-  area = {};//Traer las correspondientes segun permisos
+  area = {};
   areas: any = [];
   usuarioLogueado = "";
   @ViewChild(ProyectosPorPlanComponent, {static: true}) proyectosPorPlan: ProyectosPorPlanComponent;
   
-  constructor(private proyectosService: ProyectosService, private planesService: PlanesService, private userService: UserService, private authentication: AutenticacionService) {
+  constructor(private proyectosService: ProyectosService, private planesService: PlanesService, private userService: UserService, private authentication: AutenticacionService, private organigramaService: OrganigramaService, private permissionsService: NgxPermissionsService) {
     this.planesService.getPlanes().subscribe(planes =>{
       this.planes = planes;
       this.plan = planes.slice(-1)[0];
@@ -35,25 +36,22 @@ export class ProyectosComponent implements OnInit {
   
   seleccionPlan(plan){
     this.plan = plan;
-    this.proyectosService.getAreasPorPlan(this.plan._id).subscribe((areas: any) =>{
-      this.areas = areas;
-      this.usuarioLogueado = this.authentication.getUsuarioLogeado();
-      this.userService.getUsers().subscribe((users: any) =>{//Busco y asigno el area del usuario logueado
-        for (let i = 0; i < users.length; i++) {
-          const user = users[i];
-          
-          if(user.username == this.usuarioLogueado){
-            for (let a = 0; a < areas.length; a++) {
-              const area = areas[a];
-              
-              if(area.idOrganigrama == user.jurisdiccion){
-                this.area = area;
-                this.proyectosPorPlan.getProyectos(this.plan,this.area);
-              }
-            }
+    let userAreaPoa = this.userService.getJurisdiccionPOA();
+
+    this.proyectosService.getAreasPorPlan(this.plan._id).subscribe((areas: any) =>{//Traer todas las areas del plan
+      for (let i = 0; i < areas.length; i++) {
+        const area = areas[i];
+        
+        if(area.idOrganigrama == userAreaPoa){
+          this.area = area;
+          if(this.permissionsService.getPermission("poa.verJurisdiccion")){//Si el user pertenece a un area ve solo esa area
+            this.areas = [area];
+          }else if(this.permissionsService.getPermission("poa.administrador")){//Si el user es admin ve todas las areas
+            this.areas = areas;
           }
+          this.proyectosPorPlan.getProyectos(this.plan,this.area);
         }
-      })
+      }
     });
   }
 
